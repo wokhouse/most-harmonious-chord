4212caaf28bfb62ac5106a76bb20a42a2fa1bb26
Jasper Fung <jsprfng@gmail.com>
2017-04-23 23:48:13 -0700

    ✨created spec for checking input types

---
+++ chord.js
@@ -0,0 +1,43 @@
+// was having weird issues with promise chain, so disabled this rule.
+/* eslint-disable comma-dangle */
+const Promise = require('bluebird')
+
+function checkLength(inputNotesArray) {
+	return new Promise((resolve, reject) => {
+		const maxArrayLength = 11
+		if (inputNotesArray.length > maxArrayLength) return reject(`array too long, max length ${maxArrayLength}`)
+		if (inputNotesArray.length < 1) return reject('array does not contain anything')
+		return resolve(inputNotesArray)
+	})
+}
+
+function checkForNonIntegerTypes(inputNotesArray) {
+	return new Promise((resolve, reject) => {
+		for (let i = inputNotesArray.length - 1; i >= 0; i -= 1) {
+			if (isNaN(inputNotesArray[i])) return reject('array contains types other than integers')
+		}
+		return resolve(inputNotesArray)
+	})
+}
+
+function checkForUniqueNumbers(inputNotesArray) {
+	return new Promise((resolve, reject) => {
+		const uniqueNumbers = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0 }
+		inputNotesArray.map(number => (uniqueNumbers[number] += 1))
+		inputNotesArray.map((number) => {
+			if (uniqueNumbers[number] > 1) return reject('array contains non-unique numbers')
+			return resolve(inputNotesArray)
+		})
+	})
+}
+
+function findChord(inputNotesArray) {
+	return new Promise((resolve, reject) => checkLength(inputNotesArray)
+		.then(checkForNonIntegerTypes)
+		.then(checkForUniqueNumbers)
+		.then(resolve)
+		.catch(reject)
+	)
+}
+
+module.exports = { findChord }
---
+++ spec/chord.spec.js
@@ -0,0 +1,60 @@
+/* global describe, it, expect */
+
+const chord = require('../chord.js')
+
+const inputNumbers = [0, 4, 7]
+
+describe('chord finder', () => {
+	describe('input verification', () => {
+		// Input up to 11 unique digits between 0 and 11 in the format of a numerically sorted list:
+		it('should take up to 11 digits, all unique, each between 0 and 11 as an array', (done) => {
+			chord.findChord(inputNumbers).then(done).catch(done.fail)
+		})
+		it('should not take more than 11 digits', (done) => {
+			const tooLongArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+			chord.findChord(tooLongArray).then(done.fail).catch((err) => {
+				expect(err).toEqual('array too long, max length 11')
+				done()
+			})
+		})
+		it('should not take less than 1 digits', (done) => {
+			const tooShortArray = []
+			chord.findChord(tooShortArray).then(done.fail).catch((err) => {
+				expect(err).toEqual('array does not contain anything')
+				done()
+			})
+		})
+		it('should reject lists with non-unique numbers', (done) => {
+			const arrayWithNonUniqueNumbers = [1, 2, 3, 4, 1]
+			chord.findChord(arrayWithNonUniqueNumbers).then(done.fail).catch((err) => {
+				expect(err).toEqual('array contains non-unique numbers')
+				done()
+			})
+		})
+		it('should reject arrays that do not contain numbers', (done) => {
+			const arrayWithNonUniqueNumbers = [1, 'a', 3, 4, 1]
+			chord.findChord(arrayWithNonUniqueNumbers).then(done.fail).catch((err) => {
+				expect(err).toEqual('array contains types other than integers')
+				done()
+			})
+		})
+	})
+	// Add value of N to each number. First time add 0. Runs 12 times, each time adding one more.
+	// Numbers should only be between 0-11.
+
+	// The computer finds (0+N, 4+N, 7+N) in the table below and outputs corresponding
+	// list of numbers.
+
+	// When N=0, Outputs: 480 600 720
+
+	// Find greatest common denominator and divide each of the indexed numbers by it.
+	// When N=0, GCD(480 600 720)=120, Outputs: 4 5 6
+
+	// Find the least common multiple (LCM) of the new set of numbers.
+	// When N=0, LCM(4,5,6)=60, Outputs: 60
+
+	// Store number. Repeat above process with N=1, N=2,... until N=11, storing all numbers.
+	// Compare all 12 numbers, outputting the smallest value of N.
+
+	// If multiple N values share the minimum value, output all of them in a list.
+})
